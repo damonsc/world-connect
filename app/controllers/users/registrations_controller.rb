91cf9ba3484c3a70b08c8405ba496714150d6e2c
damonsc <lamonte.s2146@yahoo.com>
2019-09-20 19:47:27 -0400

    update

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,9 +1,11 @@
 # frozen_string_literal: true
+ 
 class Users::RegistrationsController < Devise::RegistrationsController
+include Accessible
+  skip_before_action :check_user, except: [:new, :create]
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
-  include Accessible
-  skip_before_action :check_user, except: [:new, :create]
+
   # GET /resource/sign_up
   # def new
   #   super
