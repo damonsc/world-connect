2df51ef6cdccea50aabb05be94ab3955b203a9bf
damonsc <lamonte.s2146@yahoo.com>
2019-09-21 15:43:55 -0400

    update(9/21/19)

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -2,4 +2,7 @@ class Admin < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :validatable,:timeoutable, :trackable and :omniauthable :registerable,:recoverable,:rememberable,
   devise :database_authenticatable, :trackable, :timeoutable, :lockable, :registerable ,:recoverable
+  
+  has_many :uploads
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_21_043252) do
+ActiveRecord::Schema.define(version: 2019_09_21_184156) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -64,6 +64,8 @@ ActiveRecord::Schema.define(version: 2019_09_21_043252) do
     t.string "attachment"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_uploads_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
