226d06fb8247fae83e7c7cbf685a2539ba021e6d
damonsc <lamonte.s2146@yahoo.com>
2019-10-03 16:54:18 -0400

    update(10/3/19)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_02_194432) do
+ActiveRecord::Schema.define(version: 2019_10_03_184824) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -85,6 +85,8 @@ ActiveRecord::Schema.define(version: 2019_10_02_194432) do
     t.text "first_name"
     t.text "last_name"
     t.text "phone_number"
+    t.text "username"
+    t.text "pword"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
