815dcaeff46b27b16ec82cf31f193ecc903777a3
damonsc <54198512+damonsc@users.noreply.github.com>
2019-10-29 23:48:36 -0400

    Update codes_controller.rb

--- app/controllers/codes_controller.rb
+++ app/controllers/codes_controller.rb
@@ -69,6 +69,6 @@ class CodesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def code_params
-      params.require(:code).permit(:code)
+      params.require(:code).permit(:code, :client)
     end
 end
