9d569c292a263c713671012861733903db10111d
damonsc <54198512+damonsc@users.noreply.github.com>
2020-10-08 02:05:15 -0400

    update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -111,4 +111,19 @@ Rails.application.configure do
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 	config.action_mailer.default_url_options = { host: 'world-connect.herokuapp.com'}
 	config.force_ssl = true
+    
+      config.paperclip_defaults = { 
+     :storage => :s3, 
+    :s3_credentials => { 
+     :bucket => ENV['AWS_BUCKET'], 
+     :access_key_id => ENV['AWS_ACCESS_KEY_ID'], 
+     :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'] 
+     } 
+    }
+    
+    
 end
+
+
+  
+
