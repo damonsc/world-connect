03079379512fcdc24d291966e047633e451ac3e9
damonsc <54198512+damonsc@users.noreply.github.com>
2020-10-08 02:09:12 -0400

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -112,15 +112,6 @@ Rails.application.configure do
 	config.action_mailer.default_url_options = { host: 'world-connect.herokuapp.com'}
 	config.force_ssl = true
     
-      config.paperclip_defaults = { 
-     :storage => :s3, 
-    :s3_credentials => { 
-     :bucket => ENV['AWS_BUCKET'], 
-     :access_key_id => ENV['AWS_ACCESS_KEY_ID'], 
-     :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'] 
-     } 
-    }
-end
     
 end
 
