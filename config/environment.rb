c0dbf412ce8a002236500293e3e438584c4c09f7
damonsc <54198512+damonsc@users.noreply.github.com>
2020-11-01 21:19:26 -0500

    Update environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -6,10 +6,11 @@ Rails.application.initialize!
 
 ActionMailer::Base.smtp_settings = {
   :user_name => 'apikey',
-  :password => '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',
-  :domain => 'worldconnect.app',
+  :password => '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
+',
+  :domain => 'purecodedigital.com',
   :address => 'smtp.sendgrid.net',
-  :port => 465,
+  :port => 587,
   :authentication => :plain,
   :enable_starttls_auto => true
 }
