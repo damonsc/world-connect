20ec77938fae793191d61ef20f547662ef6ea15f
damonsc <54198512+damonsc@users.noreply.github.com>
2020-11-01 20:59:48 -0500

    Update environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -6,7 +6,7 @@ Rails.application.initialize!
 
 ActionMailer::Base.smtp_settings = {
   :user_name => 'apikey',
-  :password => 'SENDGRID_API_KEY',
+  :password => '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',
   :domain => 'worldconnect.app',
   :address => 'smtp.sendgrid.net',
   :port => 465,
