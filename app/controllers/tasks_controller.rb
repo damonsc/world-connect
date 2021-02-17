f9bfefb6bae4b300c364ab2d64017e686f25baa3
damonsc <54198512+damonsc@users.noreply.github.com>
2021-02-17 17:39:02 -0500

    Update tasks_controller.rb

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -70,7 +70,6 @@ class TasksController < ApplicationController
       if @tasks.save
         if @tasks.code == 100
            NotifierMailer.with(task: @tasks).task_email.deliver_now
-           NotifierMailer.with(task: @tasks).task_emailclient.deliver_now
               end
           if @tasks.code == 121767
               NotifierMailer.with(task: @tasks).task_email.deliver_now
