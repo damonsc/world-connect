cc356bded8c8b850a3b57907c914de82fbdd50d0
damonsc <54198512+damonsc@users.noreply.github.com>
2021-02-17 16:31:33 -0500

    Update notifier_mailer.rb

--- app/mailers/notifier_mailer.rb
+++ app/mailers/notifier_mailer.rb
@@ -3,11 +3,14 @@ class NotifierMailer < ApplicationMailer
      default from: 'worldconnect@purecodedigital.com'
  
   def task_email
-
+      <% User.all.each do |user| %>
     @tasks = params[:tasks]
+    @user = params[:user]
     @email = 'damonsc@forfam2u.com'
+    @email2 = current_user.email
     @url  = 'http://worldconnect.app/'
     mail(to: @email, subject: 'Task Notification')
+    mail(to: @user.contact_email , subject: 'Task Notification')
   end
 
 
