029dc7119424bdc6283a252c793c445432707f76
damonsc <54198512+damonsc@users.noreply.github.com>
2021-02-17 17:21:54 -0500

    update

--- app/mailers/notifier_mailer.rb
+++ app/mailers/notifier_mailer.rb
@@ -36,5 +36,10 @@ class NotifierMailer < ApplicationMailer
        mail(to: @email, subject: 'Task Notification')
      end
 
-    
+    def current_user
+      return unless session[:user_id]
+      @current_user ||= User.find(session[:user_id])
+    end
+  end
+
 end
